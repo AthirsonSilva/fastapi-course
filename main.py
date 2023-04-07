7661f0a3e4c1d364ba8f3a4519907d8f2198f69c
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-07 19:15:47 -0300

    fix: swapping Response to JSONResponse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uuid
-from fastapi import FastAPI, Response, status, Body, HTTPException
+from fastapi import FastAPI, status, HTTPException
+from fastapi.responses import JSONResponse
 from Post import Post
 from posts import posts
 
@@ -7,75 +8,78 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root() -> Response:
-    return Response(status_code=status.HTTP_200_OK, content=Body({"message": "Welcome to my blog!"}))
-
+async def root() -> JSONResponse:
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Welcome to my blog!"})
 
 @app.post("/api/v1/posts")
-async def create(request: Post) -> Response:
+async def create(request: Post) -> JSONResponse:
     post_dict = request.dict()
     post_dict["id"] = uuid.uuid4()
     posts.append(post_dict)
 
-    return Response(status_code=status.HTTP_201_CREATED, content=Body({"message": "Post created successfully!", "data": post_dict}))
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": "Post created successfully!", "data": post_dict})
 
 
 @app.get("/api/v1/posts/latest")
-async def find_latest() -> Response:
+async def find_latest() -> JSONResponse:
     post = posts[-1]
 
     if not post:
-        raise HTTPException(status_code=404, detail="Post not found!")
+        raise HTTPException(
+            status_code=404, detail=f"Post with id: {id} not found!")
 
-    return Response(status_code=status.HTTP_200_OK, content=Body({"data": post}))
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"data": post})
 
 
 @app.get("/api/v1/posts")
-async def find_all() -> Response:
-    return Response(status_code=status.HTTP_200_OK, content=Body({"data": posts}))
+async def find_all() -> JSONResponse:
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"data": posts})
 
 
 @app.get("/api/v1/posts/{id}")
-async def find_one(id: str) -> Response:
+async def find_one(id: str) -> JSONResponse:
     try:
         post = [post for post in posts if post["id"] == id]
 
         if not post:
-            raise HTTPException(status_code=404, detail="Post not found!")
-        
-        return Response(status_code=status.HTTP_200_OK, content=Body({"data": post}))
+            raise HTTPException(
+                status_code=404, detail=f"Post with id: {id} not found!")
+
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"data": post})
 
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid ID format!")
 
 
 @app.delete("/api/v1/posts/{id}")
-async def delete(id: str) -> Response:
+async def delete(id: str) -> JSONResponse:
     try:
         post = [post for post in posts if post["id"] == id]
 
         if not post:
-            raise HTTPException(status_code=404, detail="Post not found!")
+            raise HTTPException(
+                status_code=404, detail=f"Post with id: {id} not found!")
 
         posts.remove(post[0])
 
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid ID format!")
 
-    return Response(status_code=status.HTTP_204_NO_CONTENT, content=Body({"message": "Post deleted successfully!"}))
+    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content={"message": "Post deleted successfully!"})
 
 
 @app.put("/api/v1/posts/{id}")
-async def update(id: str, request: Post) -> Response:
+async def update(id: str, request: Post) -> JSONResponse:
     try:
         for index, post in enumerate(posts):
             if post["id"] == id:
                 posts[index] = request.dict()
                 posts[index]["id"] = id
 
-                return Response(status_code=status.HTTP_202_ACCEPTED, content=Body({"message": "Post updated successfully!", "data": posts[index]}))
+                return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Post updated successfully!", "data": posts[index]})
 
-        raise HTTPException(status_code=404, detail="Post not found!")
+        raise HTTPException(
+            status_code=404, detail=f"Post with id: {id} not found!")
 
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid ID format!")
