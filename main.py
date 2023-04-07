75c559158cf503e515d6c953bb0ce7c664a55143
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-07 19:03:08 -0300

    refactor: adding proper response and exception classes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uuid
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, Body, HTTPException
 from Post import Post
 from posts import posts
 
@@ -7,75 +7,75 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root() -> Response:
+    return Response(status_code=status.HTTP_200_OK, content=Body({"message": "Welcome to my blog!"}))
 
 
 @app.post("/api/v1/posts")
-async def create(request: Post):
+async def create(request: Post) -> Response:
     post_dict = request.dict()
     post_dict["id"] = uuid.uuid4()
     posts.append(post_dict)
 
-    return {"message": "Post created successfully!", "data": post_dict}
+    return Response(status_code=status.HTTP_201_CREATED, content=Body({"message": "Post created successfully!", "data": post_dict}))
 
 
 @app.get("/api/v1/posts/latest")
-async def find_latest() -> dict[str, str | dict]:
+async def find_latest() -> Response:
     post = posts[-1]
 
     if not post:
-        return {"message": "Post not found!"}
+        raise HTTPException(status_code=404, detail="Post not found!")
 
-    return {"data": post}
+    return Response(status_code=status.HTTP_200_OK, content=Body({"data": post}))
 
 
 @app.get("/api/v1/posts")
-async def find_all() -> dict[str, list[dict]]:
-    return {"data": posts}
+async def find_all() -> Response:
+    return Response(status_code=status.HTTP_200_OK, content=Body({"data": posts}))
 
 
 @app.get("/api/v1/posts/{id}")
-async def find_one(id: str) -> dict[str, str | list[dict]]:
+async def find_one(id: str) -> Response:
     try:
         post = [post for post in posts if post["id"] == id]
 
         if not post:
-            return {"message": "Post not found!"}
+            raise HTTPException(status_code=404, detail="Post not found!")
+        
+        return Response(status_code=status.HTTP_200_OK, content=Body({"data": post}))
 
     except ValueError:
-        return {"message": "Invalid ID format!"}
-
-    return {"data": post}
+        raise HTTPException(status_code=400, detail="Invalid ID format!")
 
 
 @app.delete("/api/v1/posts/{id}")
-async def delete(id: str) -> dict[str, str]:
+async def delete(id: str) -> Response:
     try:
         post = [post for post in posts if post["id"] == id]
 
         if not post:
-            return {"message": "Post not found!"}
+            raise HTTPException(status_code=404, detail="Post not found!")
 
         posts.remove(post[0])
 
     except ValueError:
-        return {"message": "Invalid ID format!"}
+        raise HTTPException(status_code=400, detail="Invalid ID format!")
 
-    return {"message": "Post deleted successfully!"}
+    return Response(status_code=status.HTTP_204_NO_CONTENT, content=Body({"message": "Post deleted successfully!"}))
 
 
 @app.put("/api/v1/posts/{id}")
-async def update(id: str, request: Post) -> dict[str, str | dict]:
+async def update(id: str, request: Post) -> Response:
     try:
         for index, post in enumerate(posts):
             if post["id"] == id:
                 posts[index] = request.dict()
                 posts[index]["id"] = id
 
-                return {"message": "Post updated successfully!", "data": posts[index]}
+                return Response(status_code=status.HTTP_202_ACCEPTED, content=Body({"message": "Post updated successfully!", "data": posts[index]}))
 
-        return {"message": "Post not found!"}
+        raise HTTPException(status_code=404, detail="Post not found!")
 
     except ValueError:
-        return {"message": "Invalid ID format!"}
+        raise HTTPException(status_code=400, detail="Invalid ID format!")
