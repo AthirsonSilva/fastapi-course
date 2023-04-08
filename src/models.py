988c74a0fd26a0fe2b66ce24e99bd2e35eb51334
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-08 13:35:19 -0300

    feat: setting created_at and updated_at columns

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import uuid
+from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, Boolean, UUID, Text, text
+from sqlalchemy import Column, Integer, String, Boolean, UUID, Text, text, TIMESTAMP
 
 from .database import Base
 
@@ -13,3 +14,5 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, nullable=False, default=False, server_default="TRUE")
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, default=datetime.utcnow, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, default=datetime.utcnow, server_default=text("now()"))
