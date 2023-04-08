56f61384febd2526033b4e0422869af25ab35d73
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-08 13:32:18 -0300

    feat: setting id column default as uuid4

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, Integer, String, Boolean, UUID
+from sqlalchemy import Column, Integer, String, Boolean, UUID, Text, text
 
 from .database import Base
 
@@ -8,7 +8,8 @@ from .database import Base
 class Post(Base):
     __tablename__ = "post"
 
-    id = Column(UUID, primary_key=True, index=True, unique=True, default=uuid.uuid4)
+    id = Column(UUID, primary_key=True, index=True, unique=True, default=uuid.uuid4,
+                server_default=text("uuid_generate_v4()"))
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, nullable=False, default=False)
+    published = Column(Boolean, nullable=False, default=False, server_default="TRUE")
